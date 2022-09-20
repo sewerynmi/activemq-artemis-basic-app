7c5867d26ab855ea5112e0a2646ea974d0f7005a
sewerynmi <seweryn.michota@digital.homeoffice.gov.uk>
2022-09-20 11:25:25 +0100

    Changed docker-compose added File Upload via @Post request

--- src/main/java/com/severinu/artemisdemo/ArtemisDemoApplication.java
+++ src/main/java/com/severinu/artemisdemo/ArtemisDemoApplication.java
@@ -1,9 +1,14 @@
 package com.severinu.artemisdemo;
 
+import com.severinu.artemisdemo.config.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
 public class ArtemisDemoApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/severinu/artemisdemo/controller/MessageController.java
+++ src/main/java/com/severinu/artemisdemo/controller/MessageController.java
@@ -1,22 +1,44 @@
 package com.severinu.artemisdemo.controller;
 
 import com.severinu.artemisdemo.dto.MessageToQueueDTO;
+import com.severinu.artemisdemo.responses.UploadFileResponse;
 import com.severinu.artemisdemo.service.DispatcherService;
+import com.severinu.artemisdemo.service.FileStorageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("/message")
 public class MessageController {
 
-    private final DispatcherService dispatcherService;
+    private static final Logger logger = LoggerFactory.getLogger(MessageController.class);
 
-    public MessageController(DispatcherService dispatcherService) {
+    private final DispatcherService dispatcherService;
+    private final FileStorageService fileStorageService;
+
+    public MessageController(DispatcherService dispatcherService, FileStorageService fileStorageService) {
         this.dispatcherService = dispatcherService;
+        this.fileStorageService = fileStorageService;
+    }
+
+    @PostMapping("/send/file")
+    public UploadFileResponse sendFile (@RequestParam("file") MultipartFile file) {
+        logger.info("Calling method sendFile");
+        String fileName = fileStorageService.storeFile(file);
+        logger.info("Attempt to upload file with name: " + fileName);
+
+        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/downloadFile/")
+                .path(fileName)
+                .toUriString();
+
+        return new UploadFileResponse(fileName, fileDownloadUri,
+                file.getContentType(), file.getSize());
     }
 
     @PostMapping("/send")
